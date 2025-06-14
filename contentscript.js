5716e8af65de0e4cd2c0343c0bcb26218fee9b3f
Ankit Raj <148187070+chalakbilla@users.noreply.github.com>
2025-06-14 14:32:38 +0530

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -1,9 +1,47 @@
 (() => {
+  function findLargestPlayingVideo() {
+    const videos = Array.from(document.querySelectorAll('video'))
+      .filter(video => video.readyState !== 0)
+      .filter(video => video.disablePictureInPicture === false)
+      .sort((v1, v2) => {
+        const v1Rect = v1.getClientRects()[0] || { width: 0, height: 0 };
+        const v2Rect = v2.getClientRects()[0] || { width: 0, height: 0 };
+        return (v2Rect.width * v2Rect.height) - (v1Rect.width * v1Rect.height);
+      });
+
+    return videos[0];
+  }
+
+  async function requestPictureInPicture(video) {
+    try {
+      await video.requestPictureInPicture();
+      video.setAttribute('__pip__', true);
+      video.addEventListener('leavepictureinpicture', () => {
+        video.removeAttribute('__pip__');
+      }, { once: true });
+
+      new ResizeObserver(maybeUpdatePictureInPictureVideo).observe(video);
+    } catch (error) {
+      console.log("Error entering PiP:", error);
+    }
+  }
+
+  function maybeUpdatePictureInPictureVideo(entries, observer) {
+    const observedVideo = entries[0].target;
+    if (!document.querySelector('[__pip__]')) {
+      observer.unobserve(observedVideo);
+      return;
+    }
+    const newVideo = findLargestPlayingVideo();
+    if (newVideo && !newVideo.hasAttribute('__pip__')) {
+      observer.unobserve(observedVideo);
+      requestPictureInPicture(newVideo);
+    }
+  }
+
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.message === "enterPictureInPicture") {
-      const video = document.querySelector("video");
-      console.log(video);
-
+      const video = findLargestPlayingVideo();
       if (!video) {
         console.log("No video element found!");
         sendResponse("no video");
@@ -15,16 +53,17 @@
           console.log("Error while exiting Picture-in-Picture:", error);
         });
       } else {
-        video.requestPictureInPicture().catch(error => {
-          console.log("Error while entering Picture-in-Picture:", error);
-        });
+        requestPictureInPicture(video);
 
+        // Media Session using nexttrack and previoustrack
         navigator.mediaSession.setActionHandler("nexttrack", () => {
           video.currentTime += 10;
+          console.log("Skipped forward 10 seconds");
         });
 
         navigator.mediaSession.setActionHandler("previoustrack", () => {
           video.currentTime -= 10;
+          console.log("Skipped backward 10 seconds");
         });
 
         navigator.mediaSession.setActionHandler("play", () => {
@@ -35,11 +74,6 @@
           } else if (video.paused) {
             video.play().catch(() => {});
             console.log("Fallback: video.play()");
-          } else {
-            try {
-              myMediaElement?.play();
-              console.log("Fallback: myMediaElement.play()");
-            } catch (e) {}
           }
         });
 
@@ -51,17 +85,12 @@
           } else if (!video.paused) {
             video.pause();
             console.log("Fallback: video.pause()");
-          } else {
-            try {
-              myMediaElement?.pause();
-              console.log("Fallback: myMediaElement.pause()");
-            } catch (e) {}
           }
         });
       }
-    }
 
-    sendResponse("done");
-    return true;
+      sendResponse("done");
+      return true;
+    }
   });
 })();
